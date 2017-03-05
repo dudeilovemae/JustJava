26b62c2be8adcfa81bce9b8b90e2d38a87e36e88
dudeilovemae <dudeilovemae@gmail.com>
2017-03-05 16:24:29 -0700

    added button functions

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,14 +9,17 @@ package com.example.android.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import java.text.NumberFormat;
 
+import static android.R.attr.order;
+import static android.R.id.message;
 
-/**
- * This app displays an order form to order coffee.
- */
+
+//This app displays an order form to order coffee.
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
@@ -27,36 +30,79 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * This method is called when the order button is clicked.
-     */
+
+    //This method is called when the order button is clicked.
     public void submitOrder(View view) {
-        display(quantity);
-        displayPrice(quantity * 5);
+
+        CheckBox whippedCheck = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCheck.isChecked();
+
+        CheckBox chocolateCheck = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheck.isChecked();
+
+        int price = calculatePrice();
+        displayMessage(createOrderSummary(price, hasWhippedCream, hasChocolate ));
+
+    }
+
+    /**
+     * Calculates the price of the order.
+     *
+     * @return total price
+     */
+    private int calculatePrice() {
+        int price = quantity * 5;
+        return price;
+    }
+
+    /**
+     * Create summary of the order
+     *
+     * @param price price of the order
+     * @param addWhippedCream do they want whipped cream
+     * @param addChocolate do they want Chocolate
+     * @return message
+     */
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate) {
+
+
+
+
+        String message = "Name: Ray Franco";
+        message = message + "\nAdd whipped cream? " + addWhippedCream;
+        message = message + "\nAdd Chocolate? " + addChocolate;
+        message = message + "\nQuantity: " + quantity;
+        message = message + "\nTotal: $" + price;
+        message = message + "\nThank You!";
+
+        return message;
     }
 
     public void increment(View view) {
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public void decrement(View view) {
         quantity = quantity - 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
+
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int inputNumber) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + inputNumber);
     }
+
     /**
-     * This method displays the given price on the screen.
+     * This method displays the given text on the screen.
      */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
+
 }
